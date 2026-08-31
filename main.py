0ed160a1f2d90bd96bea18d919adc6b5eb7bee99
DESKTOP-RJQQ2BC\user1 <aahh412@naver.com>
2026-08-31 11:13:36 +0900

    기본틀 잡기

---
+++ main.py
@@ -0,0 +1,51 @@
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+plt.rcParams["font.family"] = "Malgun Gothic"
+plt.rcParams["axes.unicode_minus"] = False
+
+from sklearn.datasets import fetch_california_housing
+
+data = fetch_california_housing()
+X = data.data
+y = data.target
+
+df = pd.DataFrame(X, columns=data.feature_names)
+
+# 주택 가격 추가
+df["MedHouseVal"] = y
+print(df.head())
+
+# csv 저장
+df.to_csv("california_housing.csv", index=False)
+print(df.shape)
+print(df.describe())
+print(df.isnull().sum())
+plt.figure(figsize=(8, 5))
+
+# 가격 분포 확인
+
+plt.hist(df["MedHouseVal"], bins=30)
+
+plt.title("Distribution of House Prices")
+plt.xlabel("House Price")
+plt.ylabel("Frequency")
+
+plt.show()
+# x y 설정
+X = df.drop("MedHouseVal", axis=1)
+y = df["MedHouseVal"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    random_state=42, test_size=0.2)
+
+# 모델
+m = LinearRegression()
+# 학습
+m.fit(x_train, y_train)
+# 예측
+y_pred = m.predict(x_test)
